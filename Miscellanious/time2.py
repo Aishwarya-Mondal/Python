a5cafaff5715b5b1b4ada2527cfa0ed312962bc6
Aishwarya Mondal <72487093+Aishwarya-Mondal@users.noreply.github.com>
2022-06-13 13:01:01 +0530

    Delete time2.py

--- Miscellanious/time2.py
+++
@@ -1,7 +0,0 @@
-import time
-print(time.localtime(time.time()))
-tuple1 = (2000,10,27,9,10,0,4,0,0)
-print(time.localtime(time.mktime(tuple1)))
-
-time.sleep(5) #5sec delay
-print("Hello this is time.py")
