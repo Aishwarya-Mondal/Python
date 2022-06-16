92107ee43620b5989dcbff518e198fd7b064ef6f
Aishwarya Mondal <72487093+Aishwarya-Mondal@users.noreply.github.com>
2022-06-16 11:06:38 +0530

    Add files via upload

---
+++ Exception/assertion.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a Number:"))
+#if statement is false then execute the line
+assert n>18, "Not eligible for college" 
+
+print("Eligible for college") #if statement is true execute this line
